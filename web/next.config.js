1bf9ac9a58b5fadd0f0ba8f1f0671ced9def9038
Ritesh Regmi <riteshregmi85@gmail.com>
2023-04-22 17:24:51 +0545

    autoprefixer

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   experimental: {
     appDir: true,
     
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        // don't do anything for client-side code
+        return config;
+      }
+  
+      // enable __dirname for server-side code
+      config.node = {
+        __dirname: true,
+      };
+  
+      return config;
+    },
+
+    
   },
 }
 
