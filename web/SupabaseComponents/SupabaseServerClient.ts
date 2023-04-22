71cddae1d5bdd860cb90455c2ac8e79143b96778
Ritesh Regmi <riteshregmi85@gmail.com>
2023-04-22 21:47:09 +0545

    sdf

--- web/SupabaseComponents/SupabaseServerClient.ts
+++
@@ -1,9 +0,0 @@
-import { headers, cookies } from 'next/headers'
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
-
-
-export const createClient = () =>
-  createServerComponentSupabaseClient({
-    headers,
-    cookies,
-  })
--- web/app/(protected)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-6xl text-center text-gray-700">
-        Free Fund to me
-      </h1>
-    </div>
-  );
-}
