30c03255acb8ac4f228a825e6305d8b720c89c02
Ritesh Regmi <riteshregmi85@gmail.com>
2023-04-22 12:36:26 +0545

    authsetup

--- web/app/(protected)/page.tsx
+++ web/app/(protected)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-6xl text-center text-gray-700">
-        Free Fund
+        Free Fund to me
       </h1>
     </div>
   );
