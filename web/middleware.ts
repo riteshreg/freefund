89ef24c7cf99e691af1256db9e8682e67db4e92a
Ritesh Regmi <riteshregmi85@gmail.com>
2023-04-22 13:35:25 +0545

    df

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,32 +1,44 @@
+// import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
+// import { NextResponse } from 'next/server'
+
+// import type { NextRequest } from 'next/server'
+
+// export async function middleware(req: NextRequest) {
+  
+//   const res = NextResponse.next()
+//   const supabase = createMiddlewareSupabaseClient({ req, res })
+
+//   const {
+//     data: { session },
+//   } = await supabase.auth.getSession()
+
+  
+//   if (session?.user.email?.endsWith('.com')) {
+//     // Authentication successful, forward request to protected route.
+//     return res
+//   }
+//   const redirectUrl = req.nextUrl.clone()
+//   redirectUrl.pathname = '/login'
+//   redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
+//   return NextResponse.redirect(redirectUrl)
+// }
+
+
+// export const config = {
+//     matcher: '/',
+//   }
+
+
 import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
-
 import type { NextRequest } from 'next/server'
 
+
 export async function middleware(req: NextRequest) {
-  
   const res = NextResponse.next()
   const supabase = createMiddlewareSupabaseClient({ req, res })
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
-
-  
-  if (session?.user.email?.endsWith('.com')) {
-    // Authentication successful, forward request to protected route.
-    return res
-  }
-  const redirectUrl = req.nextUrl.clone()
-  redirectUrl.pathname = '/login'
-  redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
-  return NextResponse.redirect(redirectUrl)
+  await supabase.auth.getSession()
+  return res
 }
-
-
-export const config = {
-    matcher: '/',
-  }
-
 
  
