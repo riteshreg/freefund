d1da620438ae8620767a069cafe9fd6ce5fc986b
Dipak99990 <dipaksrm98@gmail.com>
2023-04-29 00:28:51 +0545

    diipak commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,39 +1,61 @@
-'use client'
+"use client";
 
-import { useSupabase } from "@/SupabaseComponents/SupabaseProvider"
-import { useRouter } from "next/navigation"
+import { useSupabase } from "@/SupabaseComponents/SupabaseProvider";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+export default function Navbar() {
+  const { supabase } = useSupabase();
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid"
-export default function Navbar(){
+  async function handleLogout() {
+    const { error } = await supabase.auth.signOut();
+  }
+  const [scrollY, setScrollY] = useState(0);
 
-    const {supabase} = useSupabase()
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-
-   async function handleLogout(){
-        const {error} = await supabase.auth.signOut()
-    }
-
-    return(
-      <>
+  return (
+    <>
       <header>
-      <div className="flex flex-row justify-evenly items-center py-10 mx-auto min-w-screen bg-white shadow-sm">
-    <div className="flex justify-start items-center space-x-3 ">
-      <div className="border-b flex">
-     <MagnifyingGlassIcon className="w-9"/>
-        <input type="text" className="outline-none w-20" placeholder="Search.." />
-      </div>
-      <div className="">Individuals</div>
-      <div className="">Charities</div>
-    </div>
-    <div className="flex-1 text-center text-4xl">Free-Fund</div>
-    <div className="mx-8">Login</div>
-  </div>
+        <div
+          className={
+            scrollY
+              ? ` bg-white fixed  h-28 overflow-hidden flex items-center justify-between px-4 shadow-md min-w-full`
+              : `  bg-white absolute left-32 right-32 h-24 rounded-full my-8
+          overflow-hidden flex items-center justify-between px-4 shadow-md`
+          }
+        >
+          {" "}
+          <div className=" flex gap-3 text-xl">
+            <div className="flex gap-2 hover_box">
+              <div className="">
+                <MagnifyingGlassIcon className="w-7" />
+              </div>
+              <div>Search</div>
+            </div>
+            <div className="hover_box">For Individuals</div>
+            <div className="hover_box">For Charity</div>
+          </div>
+          <div className="text-5xl text-bold">Free-Fund</div>
+          <div className="flex gap-3 text-xl items-center">
+            <div className="hover_box">How It Works</div>
+            <div className="hover_box">Sign In</div>
+            <div className="outline outline-blue rounded-full  outline-2 px-6 py-1 hover:bg-blue">
+              Get Started
+            </div>
+          </div>
+        </div>
       </header>
-     
-
-
-
-      </>
-    )
+    </>
+  );
 }
