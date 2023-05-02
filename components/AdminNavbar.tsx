c4ae0ba0b4b6d5b6617add06a4a86d9ded6a6cee
Ritesh Regmi <riteshregmi85@gmail.com>
2023-05-02 22:00:16 +0545

    admin layout

---
+++ components/AdminNavbar.tsx
@@ -0,0 +1,8 @@
+export default function AdminNavbar(){
+    return(
+        <div className="flex text-white justify-around bg-green-700 px-3 py-2">
+            <h1>Admin</h1>
+            <h1>Profile</h1>
+        </div>
+    )
+}
---
+++ components/AdminSidebar.tsx
@@ -0,0 +1,8 @@
+export default function AdminSidebar(){
+    return(
+        <div className="h-screen bg-green-800">
+            <h1>Sidebar</h1>
+
+        </div>
+    )
+}
