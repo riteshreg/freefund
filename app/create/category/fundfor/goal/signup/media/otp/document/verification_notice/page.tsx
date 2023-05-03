995ab71ebee8f850c66474633e79673430949e7b
Ritesh Regmi <riteshregmi85@gmail.com>
2023-05-03 21:43:31 +0545

    create_page_flow_complete

---
+++ app/create/category/fundfor/goal/signup/media/otp/document/verification_notice/page.tsx
@@ -0,0 +1,7 @@
+export default function Verification(){
+    return(
+        <div>
+            <h1 className="text-3xl text-gray-600 max-w-3xl ml-20">{`Our Team will verified your request. Please receive our video call on what's app. comming any time`}</h1>
+        </div>
+    )
+}
