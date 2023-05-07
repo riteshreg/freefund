65c0233d3259c8095f19d061704b542a4e2b790b
Dipak99990 <dipaksrm98@gmail.com>
2023-05-07 23:15:44 +0545

    how-it-work

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ import { usePathname, useRouter } from "next/navigation";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 function ReturnCssToNavbar(pathArray: Number, scrollY?: Number) {
   switch (pathArray) {
@@ -13,21 +14,26 @@ function ReturnCssToNavbar(pathArray: Number, scrollY?: Number) {
       break;
     case 4:
       return "bg-blue-700";
-      break
-    default: return 'h-28 overflow-hidden flex items-center justify-between px-4 shadow-md min-w-full'
+      break;
+    default:
+      return "h-28 overflow-hidden flex items-center justify-between px-4 shadow-md min-w-full";
   }
 }
 
 export default function Navbar() {
   const { supabase } = useSupabase();
   const currentPath = usePathname();
-
+  const router = useRouter();
   const pathArray = currentPath.split("/");
   console.log("patharray", "length", pathArray, pathArray.length);
 
   const [scrollY, setScrollY] = useState(0);
   const [isExpanded, setIsExpanded] = useState(false);
   const [navisExtended, setisnavExtended] = useState(false);
+  const handleHowItWorks = () => {
+    console.log("clicked");
+    router.push("/how-it-works");
+  };
   const handleSearch = () => {};
 
   useEffect(() => {
@@ -45,7 +51,7 @@ export default function Navbar() {
   async function handleLogout() {
     const { error } = await supabase.auth.signOut();
   }
-  if (pathArray[1] === "discover") {
+  if (pathArray[1] === "discover" || pathArray[1] === "how-it-works") {
     return (
       <div className="  overflow-hidden flex items-center justify-evenly px-4 shadow-md  z-50 min-w-full h-28">
         <div className=" flex   gap-3 text-xl">
@@ -60,7 +66,9 @@ export default function Navbar() {
         </div>
         <div className="text-5xl text-bold">Free-Fund</div>
         <div className="flex gap-3 text-xl items-center">
-          <div className="hover_box">How It Works</div>
+          <div className="hover_box">
+            <button onClick={handleHowItWorks}>How It Works</button>{" "}
+          </div>
           <div className="hover_box">Sign In</div>
           <button className="outline outline-green-700 rounded-md  outline-2 px-6 py-1 hover:bg-green-900 hover:text-white">
             Start a Go Fund Me
@@ -92,7 +100,9 @@ export default function Navbar() {
                 : `absolute left-32 right-32  rounded-full my-8 overflow-hidden flex items-center justify-between px-4 shadow-md z-50 `)
             }
            
-          ${pathArray.length>2 && ReturnCssToNavbar(pathArray.length)}                   
+          ${
+            pathArray.length > 2 && ReturnCssToNavbar(pathArray.length)
+          }                   
             
           bg-white
           transition-width duration-500 ease-in-out
@@ -112,7 +122,9 @@ export default function Navbar() {
           </div>
           <div className="text-5xl text-bold">Free-Fund</div>
           <div className="flex gap-3 text-xl items-center">
-            <div className="hover_box">How It Works</div>
+            <div className="hover_box">
+              <button onClick={handleHowItWorks}>How It Works</button>
+            </div>
             <div className="hover_box">Sign In</div>
             <div className="outline outline-blue rounded-full  outline-2 px-6 py-1 hover:bg-blue">
               Get Started
